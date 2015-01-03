bb3e4d12e79fc082f2b0f373e30252e89bb9f393
sergey <serge.dmitriev@gmail.com>
2015-01-03 13:34:37 +0700

    make steps

--- handler.js
+++ handler.js
@@ -14,67 +14,134 @@ var filename = null,
 var f = new finder();
 
 
+var recognize = function (filepath, debug, callback) {
+    yandex_speech.ASR({
+        developer_key: config.yandex_key,
+        file: filepath,
+        filetype: 'audio/x-wav'
+        }, function (err, httpResponse, xml) {
+            if (debug) console.log(httpResponse.statusCode, xml);
+            callback(err, {
+                code: httpResponse.statusCode, 
+                data: xml
+            })
+        });
+}
+
+
+var Q = function (context, debug) {
+
+    var answer = function (callback) {
+        context.answer(function (err, result) {
+            if (debug) console.log(err, result);            
+            callback(err, result);
+        });
+    };
+
+    var end = function (callback) {
+        context.end();
+        callback();
+    }
+
+    var dial = function (peername, callback) {
+        context.dial('SIP/' + peername, function (err, result) {
+            if (debug) console.log(err, result);
+            callback(err, result);
+        });
+    };
+
+    var sayDigits = function (digits, escape_digits, callback) {
+        context.sayDigits(digits, escape_digits, function (err, result) {
+            if (debug) console.log(err, result);
+            callback(err, result);
+        });
+    };
+
+    var recordFile = function (filepath, type, escape_digits, duration, callback) {
+        context.recordFile(filename, type, escape_digits, duration, function (err, result) {
+            if (debug) console.log(err, result);
+            callback(err, result);
+        });
+    };
+
+    return {
+        answer: answer,
+        dial: dial,
+        end: end,
+        sayDigits: sayDigits,
+        recordFile: recordFile
+    };
+};
+
+
+
+
 var handler = function (context, debug) {
-    context.on('variables', function (vars) {
+    context.on('variables', function (vars) {        
+
         if (debug) console.log(vars);
 
-        context.answer(function (err, result) {
-            if (debug) console.log(err, result);
-            
-            context.sayDigits("1", '#', function (err, result) {
-                if (debug) console.log(err, result);
-                filename = config.directory + '/' + uuid.v4();
-                
-                context.recordFile(filename, type, '#', 10, function (err, result) {
-                if (debug) console.log(err, result);
+        var q = new Q(context, debug);
 
-                    yandex_speech.ASR({
-                        developer_key: config.yandex_key,
-                        file: filename + '.' + type,
-                        filetype: 'audio/x-wav'
-                        }, function (err, httpResponse, xml) {
+        var stepStart = function () {
+
+            q.answer(function (err, result) {
+
+                q.sayDigits("1", '#', function (err, result) {
+
+                    filename = config.directory + '/' + uuid.v4();
+                    
+                    q.recordFile(filename, type, '#', 10, function (err, result) {
+
+                        recognize(filename + '.' + type, debug, function (err, result) {
                             if (!err) {
-                                if (debug) console.log(httpResponse.statusCode, xml)
+                                stepParseRecognize(result.data, function(){
+                                    f.lookup(res, function (err, peername) {
+                                        if (debug) console.log(err, peername);
 
-                                parser.parseString(xml, function (err, result) {
-                                    if (debug) console.log('parse xml callback', err, result);
-                                    var success = result.recognitionResults.$.success;
-                                    console.log(success, success === '1');
-                                    if(success === '1') {
-                                        var res = result.recognitionResults.variant[0]._;
+                                        if (!err) {
+                                            if (debug) console.log(peername);
 
-                                        f.lookup(res, function (err, peername) {
-                                            if (debug) console.log(err, peername);
+                                            context.sayDigits(peername, '#', function (err, result) {
+                                                if (debug) console.log(err, result);
 
-                                            if (!err) {
-                                                if (debug) console.log(peername);
 
-                                                context.sayDigits(peername, '#', function (err, result) {
-                                                    if (debug) console.log(err, result);
+                                                q.dial(peername);
+                                            });
+                                        } else {
+                                            context.end()
+                                        }
 
-                                                    context.dial('SIP/' + peername, function (err, result) {
-                                                        if (debug) console.log(err, result);
+                                    });
 
-                                                        context.end();
-                                                    });
-                                                });
-                                            } else {
-                                                context.end()
-                                            }
-
-                                        });
-
-                                    } else {
-                                        if(debug) console.log('no success');
-                                        context.end();
-                                    }
                                 });
                             }
-                        }
-                    );
+                        });
+                    });
                 });
             });
-        });
+        }
+
+        var stepParseRecognize = function(data, callback){
+            parser.parseString(data, function (err, result) {
+
+                if (debug) console.log('parse xml callback', err, result);
+                var success = result.recognitionResults.$.success;
+
+                if (debug) console.log(success, success === '1');
+
+                if(success === '1') {
+                    var res = result.recognitionResults.variant[0]._;
+                    callback(null, res);
+
+                } else {
+                    if(debug) console.log('no success');
+                    stepStart();
+                }
+            });
+        };
+
+        stepStart();
     });
 }
 
