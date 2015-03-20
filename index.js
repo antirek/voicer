60b3c1c144d6262ccaac02bf10affb9a3eac6bc9
Sergey Dmitriev <serge.dmitriev@gmail.com>
2015-03-20 15:15:37 +0700

    add config.js validation

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
 
 var dingDong = require('ding-dong');
+var Joi = require('joi');
 
 var Handler = require('./lib/handler');
 var Logger = require('./lib/logger');
+var ConfigSchema = require('./lib/configSchema');
 
 var SourceFactory = require('./lib/source/sourceFactory');
 var RecognizerFactory = require('./lib/recognize/recognizerFactory');
 
 var Server = function (config) {
 
-    this.start = function () {
+    var logger;
 
+    var log = function (text, object) {
+        if (logger) {
+            logger.info(text, object);
+        } else {
+            console.log(text, object);
+        }
+    };
+
+    var validate = function (callback) {
+        Joi.validate(config, ConfigSchema, callback);
+    };
+
+    var init = function () {        
         var source = (new SourceFactory(config['lookup'])).make();
         var recognizer = (new RecognizerFactory(config['recognize'])).make();
 
         var handler = new Handler(source, recognizer, config);
 
         if (config['logger']) {
-            var logger = new Logger(config['logger']);
+            logger = new Logger(config['logger']);
             handler.setLogger(logger);
         }
 
@@ -25,9 +40,18 @@ var Server = function (config) {
             .createServer(handler.handle)
             .listen(config.server['port']);
 
-        if (logger) {
-            logger.info('server started');
-        }
+        log('server started');
+    };
+
+    this.start = function () {
+        validate(function (err, value) {
+            if (err) {
+                log('config.js have errors', err);
+            } else {
+                log('config.js validated successfully!');
+                init();    
+            }
+        });
     };
 };
 
